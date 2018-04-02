5485c73f886dcbd26a9eb4eaa30ec97d2def5505
knickkennedy <knicholas.kennedy@gmail.com>
2018-04-02 13:45:58 -0400

    Added beginning of sorts and also some mild formatting

--- app/src/main/java/cmsc355/group/moms_warehouse/addWarehouse.java
+++ app/src/main/java/cmsc355/group/moms_warehouse/addWarehouse.java
@@ -26,10 +26,11 @@ public class addWarehouse extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_warehouse);
 
-        //testing other way to implement database
         Button warehouseScreen = findViewById(R.id.Badditem);
         warehouseScreen.setOnClickListener(new View.OnClickListener(){
+
             public void onClick(View view){
+
                 Intent myIntent = new Intent(addWarehouse.this, warehouse.class);
                 addWarehouse.this.startActivity(myIntent);
 
@@ -49,72 +50,4 @@ public class addWarehouse extends AppCompatActivity {
             }
         });
     }
-    /*public void onButtonClick(View v) {
-        if (v.getId() == R.id.Badditem) {
-            Intent i = new Intent(addWarehouse.this, warehouse.class);
-            startActivity(i);
-
-            //DATABASE
-            //This section reads in the values from the text fields and puts them into the database
-            DatabaseReference db = FirebaseDatabase.getInstance().getReference("warehouseData");
-            db.addChildEventListener(new ChildEventListener() {
-
-                @Override
-                public void onChildAdded(DataSnapshot snap, String s) {
-                    warehouseData data = snap.getValue(warehouseData.class);
-                    if (data.name.equals("test1")) {
-                        System.out.printf("name: %s, desc: %s\n", data.name, data.description);
-                    }
-                }
-
-                @Override
-                public void onChildChanged(DataSnapshot dataSnapshot, String s) {
-
-                }
-
-                @Override
-                public void onChildRemoved(DataSnapshot dataSnapshot) {
-
-                }
-
-                @Override
-                public void onChildMoved(DataSnapshot dataSnapshot, String s) {
-
-                }
-
-                @Override
-                public void onCancelled(DatabaseError databaseError) {
-
-                }
-            });
-
-            TextView text = findViewById(R.id.TFItemName);
-            String itemName = text.getText().toString();
-            text = findViewById(R.id.TFdescription);
-            String itemDescription = text.getText().toString();
-            text = findViewById(R.id.TFItemLoc);
-            String itemLocation = text.getText().toString();
-            text = findViewById(R.id.TFExpDate);
-            String itemExpire = text.getText().toString();
-            text = findViewById(R.id.TFQuantity);
-            String itemQuantity = text.getText().toString();
-
-            //EventsTable eventsTable = new EventsTable();
-            //eventsTable.addEntry(new EventData(name, category, description, date, organization, location));
-
-            warehouseData itemData = new warehouseData();
-            itemData.name = itemName;
-            itemData.description = itemDescription;
-            itemData.location = itemLocation;
-            itemData.expire = itemExpire;
-            itemData.quantity = itemQuantity;
-            db.push().setValue(itemData);
-
-//            itemData.itemName = "Mehama";
-//            itemData.description = "SFDsfsdf";
-//            db.push().setValue(itemData);
-
-        }
-    }*/
-
 }
--- app/src/main/java/cmsc355/group/moms_warehouse/createAccount.java
+++ app/src/main/java/cmsc355/group/moms_warehouse/createAccount.java
@@ -1,16 +1,79 @@
 package cmsc355.group.moms_warehouse;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class createAccount extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        Button signup_button = findViewById(R.id.Bsignup);
+        signup_button.setOnClickListener(new View.OnClickListener(){
+
+            public void onClick(View view){
+
+                EditText text = findViewById(R.id.TFname);
+                String name = text.getText().toString();
+
+                text = findViewById(R.id.TFemail);
+                String email = text.getText().toString();
+
+                text = findViewById(R.id.TFuname);
+                String user_name = text.getText().toString();
+
+                text = findViewById(R.id.TFpass1);
+                String password_one = text.getText().toString();
+
+                text = findViewById(R.id.TFpass2);
+                String password_two = text.getText().toString();
+
+                /*if(!password_one.equals(password_two)){
+                    System.out.println("You goofed!");
+                    Intent i = new Intent(createAccount.this, LoginActivity.class);
+                    startActivity(i);
+                }
+                else{
+                    create_new_user(email, password_one);
+                }*/
+            }
+        });
+    }
+
+    public void create_new_user(String email, String password){
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Log.d("Error", "createUserWithEmail:success");
+                        }
+                        else{
+                            Log.w("Error", "createUserWithEmail:failure", task.getException());
+
+                        }
+                    }
+                });
     }
 
     public void onButtonClick(View v) {
