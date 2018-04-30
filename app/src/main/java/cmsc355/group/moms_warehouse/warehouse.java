6b8bc8128e417878f7d6265d1b590cc75a04756d
knickkennedy <knicholas.kennedy@gmail.com>
2018-04-30 19:10:40 -0400

    Search Bar created and passed

--- app/src/main/java/cmsc355/group/moms_warehouse/warehouse.java
+++ app/src/main/java/cmsc355/group/moms_warehouse/warehouse.java
@@ -1,15 +1,18 @@
 package cmsc355.group.moms_warehouse;
 
+
+import android.app.SearchManager;
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.MenuItem;
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.SearchView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -20,7 +23,6 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.internal.api.FirebaseNoSignedInUserException;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -37,9 +39,11 @@ public class warehouse extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_warehouse);
+
+        Toolbar toolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(toolbar);
 
         user = FirebaseAuth.getInstance().getCurrentUser();
 
@@ -52,64 +56,75 @@ public class warehouse extends AppCompatActivity {
             textView.append("Error");
         }
 
-        ListView listView = findViewById(R.id.itemList);
-        Collections.sort(list, new SortByNameAscending());
-        final ArrayAdapter adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, list);
+            ListView listView = findViewById(R.id.itemList);
+            Collections.sort(list, new SortByNameAscending());
+            final ArrayAdapter adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, list);
 
-        listView.setAdapter(adapter);
+            listView.setAdapter(adapter);
 
-        DatabaseReference ref = FirebaseDatabase.getInstance().getReference().child("users/").child(user.getUid()).child("items");
-        ref.addChildEventListener(new ChildEventListener() {
-            @Override
-            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
-                Log.d("Results", "Item Added to List!");
+            DatabaseReference ref = FirebaseDatabase.getInstance().getReference().child("users/").child(user.getUid()).child("items");
+            ref.addChildEventListener(new ChildEventListener() {
+                @Override
+                public void onChildAdded(DataSnapshot dataSnapshot, String s) {
 
-                Item item = dataSnapshot.getValue(Item.class);
+                    Item item = dataSnapshot.getValue(Item.class);
 
-                list.add(item);
-                Collections.sort(list, new SortByNameAscending());
-                adapter.notifyDataSetChanged();
-            }
-
-            @Override
-            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
-
-            }
-
-            @Override
-            public void onChildRemoved(DataSnapshot dataSnapshot) {
-                Log.d("RESULTS", "Item Removed From List!");
-                if (list.remove(dataSnapshot.getValue(Item.class)))
-                {
+                    list.add(item);
+                    Collections.sort(list, new SortByNameAscending());
                     adapter.notifyDataSetChanged();
                 }
-            }
 
-            @Override
-            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+                @Override
+                public void onChildChanged(DataSnapshot dataSnapshot, String s) {
 
-            }
+                }
 
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
+                @Override
+                public void onChildRemoved(DataSnapshot dataSnapshot) {
+                    if (list.remove(dataSnapshot.getValue(Item.class)))
+                    {
+                        adapter.notifyDataSetChanged();
+                    }
+                }
 
-            }
-        });
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int i, long id) {
-                Item item = (Item) parent.getItemAtPosition(i);
-                Intent intent = new Intent(warehouse.this, itemDetails.class);
+                @Override
+                public void onChildMoved(DataSnapshot dataSnapshot, String s) {
 
-                intent.putExtra("name", item.getName());
-                intent.putExtra("description", item.getDescription());
-                intent.putExtra("location", item.getLocation());
-                intent.putExtra("expire", item.getExpire());
-                intent.putExtra("quantity", item.getQuantity());
+                }
 
-                startActivity(intent);
-            }
-        });
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+            });
+            listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int i, long id) {
+                    Item item = (Item) parent.getItemAtPosition(i);
+                    Intent intent = new Intent(warehouse.this, itemDetails.class);
+
+                    intent.putExtra("name", item.getName());
+                    intent.putExtra("description", item.getDescription());
+                    intent.putExtra("location", item.getLocation());
+                    intent.putExtra("expire", item.getExpire());
+                    intent.putExtra("quantity", item.getQuantity());
+
+                    startActivity(intent);
+                }
+            });
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            Toast.makeText(this, "Searching by: "+ query, Toast.LENGTH_SHORT).show();
+
+        } else if (Intent.ACTION_VIEW.equals(intent.getAction())) {
+            String uri = intent.getDataString();
+            Toast.makeText(this, "Suggestion: "+ uri, Toast.LENGTH_SHORT).show();
+        }
     }
 
     public void onButtonClick(View v) {
@@ -117,37 +132,20 @@ public class warehouse extends AppCompatActivity {
             Intent i = new Intent(warehouse.this, addWarehouse.class);
             startActivity(i);
         }
+    }
 
-        if (v.getId() == R.id.Bsearch) {
-            TextView text = findViewById(R.id.TFsearch);
-            String query = text.getText().toString();
-            //SearchView searchEntry = (SearchView) findViewById(R.id.searchItems);
-            //CharSequence query = searchEntry.getQuery();
-            //CharSequence query = "test";
-            //String itemExist = FirebaseDatabase.getInstance().getReference().child("users/").child(user.getUid()).child("items").child(query).setValue();
-            if (!query.equals("test")) {
-            //if () {
-                Toast.makeText(warehouse.this, "FAILURE!!", Toast.LENGTH_SHORT).show();
-                Log.e("queryValue", "Value: " + (String) query);
-            } else {
-                Toast.makeText(warehouse.this, "SUCCESS!!", Toast.LENGTH_SHORT).show();
-                Log.e("queryValue", "Value: " + (String) query);
-            }
-            /*searchEntry.setOnQueryTextListener(new SearchView.OnQueryTextListener(){
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.navigation, menu);
 
-                @Override
-                public boolean onQueryTextSubmit(String query) {
-                    return false;
-                }
+        SearchManager searchManager = (SearchManager)getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView = (SearchView)menu.findItem(R.id.action_search).getActionView();
 
-                @Override
-                public boolean onQueryTextChange(String newText) {
-                    return false;
-                }
-            });
-            */
-            Intent i = new Intent(warehouse.this, warehouse.class);
-            startActivity(i);
+        if (searchManager != null) {
+            searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
         }
+        searchView.setIconifiedByDefault(false);
+
+        return true;
     }
 }
